d17d6bd3bfc256614745ff030facd3f8fe1cf0ec
DavidDev25 <david.werner@david-werner.dev>
2025-04-18 18:06:00 +0200

    Implement blog list component with dynamic post rendering and routing

--- projects/blog/blog-list/blog-list.component.ts
+++ projects/blog/blog-list/blog-list.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MOCK_POSTS, BlogPost } from '../src/app/mock-posts';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-blog-list',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.scss'
 })
 export class BlogListComponent {
+  posts: BlogPost[] = MOCK_POSTS; 
 
 }

5c13f70994efe3a3ea27dc32b48d2729851755f6
DavidDev25 <david.werner@david-werner.dev>
2025-04-18 19:37:33 +0200

    Add author field to mock blog posts for better data representation

--- projects/blog/src/app/mock-posts.ts
+++ projects/blog/src/app/mock-posts.ts
@@ -2,11 +2,12 @@ export interface BlogPost {
     id: number;
     title: string; 
     content: string;
+    author: string;
 }
 
 
 export const MOCK_POSTS: BlogPost[] = [
-    { id: 1, title: 'Mein erster Blogpost', content: 'Das ist der Inhalt des ersten Posts meines Blogs mit Beispieldaten.'},
-    { id: 2, title: 'Mein zweiter Blogpost', content: 'Das ist der Inhalt des zweiten Posts meines Blogs mit Beispieldaten.'},
-    { id: 3, title: 'Mein dritter Blogpost', content: 'Das ist der Inhalt des dritten Posts meines Blogs mit Beispieldaten.'},
+    { author: 'OtakuStaffAdmin', id: 1, title: 'Mein erster Blogpost', content: 'Das ist der Inhalt des ersten Posts meines Blogs mit Beispieldaten.'},
+    { author: 'OtakuStaffAdmin', id: 2, title: 'Mein zweiter Blogpost', content: 'Das ist der Inhalt des zweiten Posts meines Blogs mit Beispieldaten.'},
+    { author: 'OtakuStaffAdmin', id: 3, title: 'Mein dritter Blogpost', content: 'Das ist der Inhalt des dritten Posts meines Blogs mit Beispieldaten.'},
 ];

bbd2e19c8f8748ac0fb12ed3a32aea25500b5072
DavidDev25 <david.werner@david-werner.dev>
2025-04-18 19:37:28 +0200

    Implement blog post component with dynamic content rendering and error handling

--- projects/blog/blog-post/blog-post.component.ts
+++ projects/blog/blog-post/blog-post.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MOCK_POSTS, BlogPost } from '../src/app/mock-posts';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-blog-post',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.scss'
 })
 export class BlogPostComponent {
+  private route = inject(ActivatedRoute)
+  post: BlogPost | undefined;
 
+  constructor() {
+    const postId = Number(this.route.snapshot.paramMap.get('id'));
+    this.post = MOCK_POSTS.find(post => post.id === postId);
+  }
 }
